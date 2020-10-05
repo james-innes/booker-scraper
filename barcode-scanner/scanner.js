83a5e2b5b04d0921d7315e2896c425d17639a961
James Innes <34094735+james-innes@users.noreply.github.com>
2020-10-05 12:48:51 +0100

    Remove Wix template from Scanner

--- barcode-scanner/scanner.js
+++ barcode-scanner/scanner.js
@@ -1,48 +1,48 @@
-// To debug this code, open wixDefaultCustomElement.js in Developer Tools.
+
 
 import Quagga from "quagga";
 var _scannerIsRunning = false;
 
 function startScanner() {
 	Quagga.init({
-			inputStream: {
-				name: "Live",
-				type: "LiveStream",
-				target: document.querySelector("#wdce-scanner-container"),
-				constraints: {
-					width: 480,
-					height: 320,
-					facingMode: "environment"
-				}
-			},
-			decoder: {
-				readers: [
-					"code_128_reader",
-					"ean_reader",
-					"ean_8_reader",
-					"code_39_reader",
-					"code_39_vin_reader",
-					"codabar_reader",
-					"upc_reader",
-					"upc_e_reader",
-					"i2of5_reader"
-				],
-				debug: {
-					showCanvas: true,
-					showPatches: true,
-					showFoundPatches: true,
-					showSkeleton: true,
-					showLabels: true,
-					showPatchLabels: true,
-					showRemainingPatchLabels: true,
-					boxFromPatches: {
-						showTransformed: true,
-						showTransformedBox: true,
-						showBB: true
-					}
-				}
+		inputStream: {
+			name: "Live",
+			type: "LiveStream",
+			target: document.querySelector("#wdce-scanner-container"),
+			constraints: {
+				width: 480,
+				height: 320,
+				facingMode: "environment"
 			}
 		},
+		decoder: {
+			readers: [
+				"code_128_reader",
+				"ean_reader",
+				"ean_8_reader",
+				"code_39_reader",
+				"code_39_vin_reader",
+				"codabar_reader",
+				"upc_reader",
+				"upc_e_reader",
+				"i2of5_reader"
+			],
+			debug: {
+				showCanvas: true,
+				showPatches: true,
+				showFoundPatches: true,
+				showSkeleton: true,
+				showLabels: true,
+				showPatchLabels: true,
+				showRemainingPatchLabels: true,
+				boxFromPatches: {
+					showTransformed: true,
+					showTransformedBox: true,
+					showBB: true
+				}
+			}
+		}
+	},
 		function (err) {
 			if (err) {
 				console.log(err);
@@ -118,10 +118,6 @@ document.getElementById("wdce-btn").addEventListener(
 	false
 );
 
-//
-
-// //
-
 const createScannerContainer = () => {
 	const scannerContainer = document.createElement('div');
 	scannerContainer.id = 'wdce-scanner-container';
@@ -134,106 +130,3 @@ const createScannerButton = () => {
 	scannerButton.type = 'button';
 	scannerButton.value = 'Start/Stop the scanner';
 }
-
-// end of my code
-
-const IMAGE_URL = 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png';
-const H2_TEXT = 'This is a Custom Element (Corvid)';
-
-const createImage = () => {
-	const imageElement = document.createElement('img');
-	imageElement.src = IMAGE_URL;
-	imageElement.id = 'wdce-image';
-	return imageElement;
-};
-
-const createH2 = () => {
-	const h2Element = document.createElement('h2');
-	h2Element.textContent = H2_TEXT;
-	h2Element.id = 'wdce-h2';
-	return h2Element;
-};
-
-const createTextContainer = () => {
-	const textContainer = document.createElement('div');
-	textContainer.id = 'wdce-text-container';
-	textContainer.appendChild(createH2());
-	textContainer.appendChild(createH3('wdce-h3-1', H3_1_TEXT));
-	textContainer.appendChild(createH3('wdce-h3-2', H3_2_TEXT));
-	return textContainer;
-};
-
-const createImageContainer = () => {
-	const imageContainer = document.createElement('div');
-	imageContainer.id = 'wdce-image-container';
-	imageContainer.appendChild(createImage());
-	return imageContainer;
-};
-
-const createStyle = () => {
-	const styleElement = document.createElement('style');
-	styleElement.innerHTML = `
-    wix-default-custom-element {
-        background-color: #fe5900;
-        display: flex;
-        height: 100%;
-        height: -moz-available;
-        height: -webkit-fill-available;
-        width: 100%;
-        justify-content: center;
-        canvas.drawing,
-        canvas.drawingBuffer {
-          position: absolute;
-          left: 0;
-          top: 0;
-		    }
-    }
-
-    #wdce-image-container {
-        width: 35%;
-        max-width: 165px;
-        display: flex;
-        margin: 0 20px 0 30px;
-        overflow: hidden;
-    }
-
-    #wdce-image {
-        width: 100%;
-        min-width: 100px;
-    }
-
-    #wdce-text-container {
-        display: flex;
-        flex-direction: column;
-        width: 65%;
-        justify-content: center;
-        max-width: 314px;
-        min-width: 200px;
-    }
-
-    #wdce-h2 {
-        font-family: 'HelveticaNeueW01-45Ligh, HelveticaNeueW02-45Ligh, HelveticaNeueW10-45Ligh, Helvetica Neue, Helvetica, Arial, メイリオ, meiryo, ヒラギノ角ゴ pro w3, hiragino kaku gothic pro, sans-serif',
-        font-size: 16px;
-        font-weight: 500;
-        letter-spacing: 0.89px;
-        color: #32536a;
-        margin: 0 0 16px 0;
-    }
-    `;
-	return styleElement;
-};
-
-class WixDefaultCustomElement extends HTMLElement {
-	constructor() {
-		super();
-	}
-
-	connectedCallback() {
-		this.appendChild(createStyle());
-		this.appendChild(createImageContainer());
-		this.appendChild(createTextContainer());
-		this.appendChild(createScannerContainer());
-		this.appendChild(createScannerButton());
-	}
-}
-customElements.define('wix-default-custom-element', WixDefaultCustomElement);
