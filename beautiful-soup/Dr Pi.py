d19ad9509f4be179c2af3582da55365193d8b5c1
James Innes <34094735+james-innes@users.noreply.github.com>
2020-10-05 11:53:13 +0100

    Put all the notes in on file

--- beautiful-soup/Dr Pi.py
+++ beautiful-soup/Dr Pi.py
@@ -1,4 +1,4 @@
-# import packages 
+# import packages
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,7 +8,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 12239.92.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.136 Safari/537.36'
 }
 
-#first booker login form 
+# first booker login form
 login_data = {
     'OutsideHomePageControl$CustomerNumber': '303182030'
 }
@@ -16,22 +16,22 @@ login_data = {
 # second booker login form - will work when we have found all hidden values
 
 user_data = {
-    '__VIEWSTATEGENERATOR': 'E026D3EE', # check - may be dynamically created
+    '__VIEWSTATEGENERATOR': 'E026D3EE',  # check - may be dynamically created
     'LoginControl$EmailSingle': 'nigelsr@gmail.com',
     'LoginControl$PasswordSingle': 'wCeFBxSnxCpkF93m',
-    'LoginControl$EnterEmailPasswordSubmit.x': '55', # may be dynamically created
-    'LoginControl$EnterEmailPasswordSubmit.y': '74' # may be dynamically created
+    'LoginControl$EnterEmailPasswordSubmit.x': '55',  # may be dynamically created
+    'LoginControl$EnterEmailPasswordSubmit.y': '74'  # may be dynamically created
 }
 
 
 with requests.Session() as s:
     url = 'https://www.booker.co.uk/account/loginregister/UserLogin.aspx'
-    response = s.post(url,login_data, headers=headers)
+    response = s.post(url, login_data, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     print('___The First Log In Page___')
-    print(soup.prettify()) # The html we need to extract hidden values from
+    print(soup.prettify())  # The html we need to extract hidden values from
     print("******")
-    hidden_values = soup.find_all("div", {"class":"aspNetHidden"})
+    hidden_values = soup.find_all("div", {"class": "aspNetHidden"})
     print(hidden_values)
 
 '''with requests.session() as s:
@@ -39,5 +39,9 @@ with requests.Session() as s:
     response = s.post(url, user_data, headers=headers)
     print(response.content)
     print("\n")'''
-    
-    # Once logged in, get robots.txt and visit all disallowed directories ;o)
+
+# Found hidden directories - can't access content inside
+
+# Can login - can't move to next page and scrap data
+
+# Once logged in, get robots.txt and visit all disallowed directories ;o)
