4acee9d762fb8de1f57267b40ffcba0983e3318b
James Innes <34094735+james-innes@users.noreply.github.com>
2020-10-05 13:00:29 +0100

    Add and sort out scanner stuff

---
+++ barcodes/scanner/generate-barcodes-file.js
@@ -0,0 +1,2 @@
+
+// input product codes/barcodes and QTY and generate barcode.txt to upload to website
---
+++ barcodes/scrapper/barcode-scrapper.py
@@ -0,0 +1,5 @@
+
+
+# Instead of using static website running on local host use sitemap.json to visit all category pages and click print list and then scrap barcodes for all the products
+
+# Seleiumn webdriver here
