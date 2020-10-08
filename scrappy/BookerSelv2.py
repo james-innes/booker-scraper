194b4075b5a8c548d22568f18f8d758dbd7443b4
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-10-08 14:42:06 +0100

    Updated with short cookies, load_dotenv
    installed python-dotenv 0.14.0 for dotenv
    used James's shortened cookies
    now uses items.py for the export pipeline to CSV (and eventual DB)
    added self.driver.quit() before def parse - as per James's code - was missing before

--- scrappy/BookerSelv2.py
+++ scrappy/BookerSelv2.py
@@ -2,14 +2,13 @@
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 #|r|e|d|a|n|d|g|r|e|e|n|.|c|o|.|u|k|
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-# ! Firefox has been used for webdriver ! #
+# ! Firefox has been used for webdriver #
 
 import os
 from selenium import webdriver
 import time
-from urllib.parse import urljoin
 
+from urllib.parse import urljoin
 from scrapy.crawler import CrawlerProcess
 from scrapy import Spider, Request
 from scrapy.http import FormRequest
@@ -17,55 +16,53 @@ from scrapy.settings import Settings
 from scrapy.shell import inspect_response
 from scrapy import Selector
 from scrapy.http import HtmlResponse
-	
-custno=os.environ.get('booker_customer')
-email=os.environ.get('booker_email')
-passw=os.environ.get('booker_password')
 
-class LoginSpider2(Spider):
+from scrapy.loader import ItemLoader
+from items import BookerMbItem
+
+from dotenv import load_dotenv
+load_dotenv()
+	
+class BookerProductList(Spider):
     header = {"User-Agent":"Mozilla/5.0 Gecko/20100101 Firefox/33.0"}
     name = "booker_mb"
-    custom_settings = {"FEEDS": {"booker.csv":{"format":"csv"}}}
+    custom_settings = {"FEEDS": {"booker1.csv":{"format":"csv"}}}
     allowed_domains = ["booker.co.uk"]
     start_urls = ['https://www.booker.co.uk/home.aspx']
     
+    try:
+        os.remove("booker1.csv")
+    except OSError:
+        pass
+    
     def __init__(self):
-		# Use  Selenium to log in and get a response for Scrapy
-        self.custno=os.environ.get('booker_customer')
-        self.email=os.environ.get('booker_email')
-        self.passw=os.environ.get('booker_password')
+
         self.driver = webdriver.Firefox()
         
         self.driver.get('https://www.booker.co.uk/home.aspx')
-        self.driver.find_element_by_id ('OutsideHomePageControl_CustomerNumber').send_keys(self.custno)
+        self.driver.find_element_by_id ('OutsideHomePageControl_CustomerNumber').send_keys(os.getenv("BOOKER_ACCOUNT"))
         self.driver.find_element_by_id('OutsideHomePageControl_cmdCustomerNumber').click()
         print(self.driver.title)
         time.sleep(2)
              
-        self.driver.find_element_by_id ('LoginControl_EmailSingle').send_keys(self.email)
-        self.driver.find_element_by_id ('LoginControl_PasswordSingle').send_keys(self.passw)
+        self.driver.find_element_by_id ('LoginControl_EmailSingle').send_keys(os.getenv('BOOKER_EMAIL'))
+        self.driver.find_element_by_id ('LoginControl_PasswordSingle').send_keys(os.getenv('BOOKER_PASSWORD'))
         self.driver.find_element_by_id('LoginControl_EnterEmailPasswordSubmit').click()
         
-    def parse(self, response):
-		# Gets the Selenium response and passes to Scrapy to parse/crawl
-        self.driver.get(response.url)
-        res = response.replace(body=self.driver.page_source)
-        logged_in_page = (res.body)
-        print('logged_in_page')
-
+        self.cookie = self.driver.get_cookie("ASP.NET_SessionId")
+        self.parse(response=self.driver.page_source)
+        self.driver.quit()
         
-        for page in res.css('a'):
-            yield {
-                'url-href': page.xpath('@href').extract(),
-                'url-text': page.css('::text').extract()
-                }
+        # For here onwards Selenium hands back to Scrapy
+        
+    def parse(self, response):
 
-        view_state = response.xpath('//*[@id="Form1"]/input[@name="__VIEWSTATE"]/@value').get() #
+        print('logged in')
 
-        products_link = res.xpath('//a[contains(@href,"/catalog/products.aspx")]/@href').get()    
+        products_link = response.xpath('//a[contains(@href,"/catalog/products.aspx")]/@href').get()    
         print(products_link)
         
-        # Logged in, and at home page
+        # Logged in and now at the home page
         
         products="https://www.booker.co.uk/catalog/products.aspx"
         next_url = products
@@ -74,7 +71,7 @@ class LoginSpider2(Spider):
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'keep-alive',
-            'Cookie': 'BookerEU=1; __utmz=127566494.1597660464.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); BookerMessage=WebsiteBulletinCheckedDate=17%2f08%2f2020+11%3a34%3a29; s_fid=58567D4770E7E84E-24F4000103726761; Commerce_TestPersistentCookie=TestCookie; Booker=Guid=%7b1406f591-9b3a-4788-8d72-0e714b102fdc%7d; Commerce_TestSessionCookie=TestCookie; ASP.NET_SessionId=hoxyuigmv3ufirmjfj0l3fqd; __utma=127566494.312950862.1597660464.1601982470.1601991696.7; __utmc=127566494; __utmv=127566494.Schools%20%2F%20Colleges%20%2F%20University; sc_loginStatus=( logout ); s_cc=true; gpv_pn=home%3Amybooker; CampaignHistory=8802,8797,8799,8798,13219,8801,10782,12379,30397,27942,25436,12460,30402,29716,12379,30397,27942,25436,12460,30402,29716,12379,30397,27942,25436,12460,30402,29716,12379,30397,27942,25436,12460,30402,29716; __utmt=1; __utmb=127566494.24.10.1601991696; s_sq=tescobookerwebprod%3D%2526c.%2526a.%2526activitymap.%2526page%253Dhome%25253Amybooker%2526link%253DProducts%2526region%253Dt1%2526pageIDType%253D1%2526.activitymap%2526.a%2526.c%2526pid%253Dhome%25253Amybooker%2526pidt%253D1%2526oid%253Dhttps%25253A%25252F%25252Fwww.booker.co.uk%25252Fcatalog%25252Fproducts.aspx%2526ot%253DA',
+            'Cookie': f"ASP.NET_SessionId={self.cookie['value']}",
             'Host': 'www.booker.co.uk',
             'Referer': 'https://www.booker.co.uk/catalog/mybooker.aspx',
             'Sec-Fetch-Dest': 'document',
@@ -88,10 +85,14 @@ class LoginSpider2(Spider):
         request = FormRequest(url=next_url, headers=headers ,callback=self.parse2)
         yield request      
              
+             
     def parse2(self, response):
     
         print("\n## Now on Products Page of 24 thumbnails menu ##")
-        print("\n## Now we will go and get BEER ##")
+        
+        print("\n## Now we will go and get all items, eg: BEER ##")
+        
+        # Trying one category here, will eventually read all 24 from JSON/sitemap
         
         BEER = 'https://www.booker.co.uk/catalog/products.aspx?categoryName=Default%20Catalog&keywords=beer&view=UnGrouped'
         
@@ -102,7 +103,7 @@ class LoginSpider2(Spider):
             'Connection': 'keep-alive',
             'Content-Length': '2660',
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'BookerEU=1; __utmz=127566494.1597660464.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); BookerMessage=WebsiteBulletinCheckedDate=17%2f08%2f2020+11%3a34%3a29; s_fid=58567D4770E7E84E-24F4000103726761; Commerce_TestPersistentCookie=TestCookie; Booker=Guid=%7b1406f591-9b3a-4788-8d72-0e714b102fdc%7d; Commerce_TestSessionCookie=TestCookie; ASP.NET_SessionId=hoxyuigmv3ufirmjfj0l3fqd; __utmc=127566494; __utmv=127566494.Schools%20%2F%20Colleges%20%2F%20University; sc_loginStatus=( logout ); s_cc=true; __utma=127566494.312950862.1597660464.1601991696.1602002332.8; __utmt=1; s_sq=%5B%5BB%5D%5D; CampaignHistory=19048,13219,28968,12979,11055,11576,10782,8803,8800,8799,25954,8801,8798,8797,25436,12460,30402,8796,25210,11055,8799,8802,8800,25954,19048,10782,8803,13219,11576,8801,28968,12979,9118,8797,8798; __utmb=127566494.3.10.1602002332; gpv_pn=browse%3Aall',
+            'Cookie': f"ASP.NET_SessionId={self.cookie['value']}",
             'Host': 'www.booker.co.uk',
             'Origin': 'https://www.booker.co.uk',
             'Referer': 'https://www.booker.co.uk/catalog/products.aspx',
@@ -114,23 +115,31 @@ class LoginSpider2(Spider):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'  
             }
         
-        request = Request(url=BEER,callback=self.beer)
-        yield request      
+        # Go from Main listing of 24 thumbnails to a specific Category ( approx 24 categories to visit )   
+        request = Request(url=BEER,callback=self.parse_product)
+        yield request     
         
-    def beer(self, response):
-        #print("\n## Now we have BEER! - here is the raw html ##")
-        #print(response.body)
+
+    def parse_product(self, response):
         
-        print("\n## Get link to each product page")
-        result = response.css("tr .info_r1 a::attr(href)").extract()
-        yield{'link':result}
-        
-        # The html looks pretty messy - Are you good with regex ?
-        # I don't know how long cookies last - they may expire and need updating in this code in due course?
-        
+        print("\n## Now Parsing individual products ##")
+        for pr in response.xpath('.//*[@class="pr"]'):
+            l = ItemLoader(item=BookerMbItem(), selector=pr, response=response)
+            l.add_xpath('BPLIC', ".//td[@class='packm']/div/text()")
+            l.add_xpath('alt', ".//img/@alt")
+            l.add_xpath('pack', ".//div[@class='pibox']/descendant::*/text()")
+            l.add_xpath('promotionText', ".//*[@class='promotionText']/descendant::*/text()")
+            l.add_xpath('price', ".//li[@class='wsp']/text()")
+            yield l.load_item()
+            
+        next_page_url =  response.xpath('//a[text()="Next >>"]//@href').get()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        if next_page_url is not None:
+            yield Request(absolute_next_page_url, callback=self.parse_product)
+            
+               
 # Main Driver 
 if __name__ == "__main__":
 	process = CrawlerProcess()
-	process.crawl(LoginSpider2)
+	process.crawl(BookerProductList)
 	process.start()
-
