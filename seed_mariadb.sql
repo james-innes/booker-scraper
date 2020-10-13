1598e89219d218e9b937782be9d6b56dc6f370de
James Innes <34094735+james-innes@users.noreply.github.com>
2020-10-13 13:51:17 +0100

    Add exmaple seed from csv

--- seed_mariadb.sql
+++ seed_mariadb.sql
@@ -1,3 +1,19 @@
 
 /* TODO Add Sitemap /*
 /* Add other smaple data maybe for tesing */
+
+
+CREATE TABLE discounts (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    expired_date DATE NOT NULL,
+    amount DECIMAL(10 , 2 ) NULL,
+    PRIMARY KEY (id)
+);
+
+LOAD DATA INFILE 'c:/tmp/discounts.csv' 
+INTO TABLE discounts 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
