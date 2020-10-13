e37dc850cb533c9ae4f24e122e803279cb7088d4
James Innes <34094735+james-innes@users.noreply.github.com>
2020-10-13 01:07:24 +0100

    Missing value

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ CONCURRENT_REQUESTS = 1
 DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
-CONCURRENT_REQUESTS_PER_IP = 
+# CONCURRENT_REQUESTS_PER_IP = 
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
