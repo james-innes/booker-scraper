748dcc20e83a149032c80afa738fde16e59924e7
James Innes <34094735+james-innes@users.noreply.github.com>
2020-10-22 20:04:40 +0100

    seperate to baroces py

--- barcodes.py
+++ barcodes.py
@@ -1,57 +1,27 @@
+      # To Do : Get Bar Codes - 165-190
 
+        Example:
+        https: // www.booker.co.uk/catalog/printbyplof.aspx?printtype = searchcategory & categoryname = cs13_200050
+        Bar Code Prefix = 'https://www.booker.co.uk/catalog/printbyplof.aspx?printtype=searchcategory&categoryname='
+        Bar Code Suffix = cat_codes(already extracted from JSON - see line 42)
 
-# Instead of using static website running on local host use sitemap.json to visit all category pages and click print list and then scrap barcodes for all the products
+        Iterate through each of the codes, and get the barcode with xpath
+	//*[@class= 'genericListItem']//img/alt/text() ?
 
+        Bar Code is also named 'alt' in the html - so we will simply call it 'barcode'
+        make 2nd table in a DB and do a join using product code as the KEY
+        Nice.
 
-selectors = [
-    {
-        "id": "categories",
-        "type": "SelectorLink",
-        "parentSelectors": ["_root"],
-        "selector": "a",
-
-    },
-    {
-        "id": "sub-categories",
-        "type": "SelectorLink",
-        "parentSelectors": ["categories"],
-        "selector": "a",
-
-    },
-    {
-        "id": "print-product-list",
-        "type": "SelectorLink",
-        "parentSelectors": ["sub-categories"],
-        "selector": "#printRow a",
-    }
-    {
-        "id": "ungrouped-proceed",
-        "type": "SelectorLink",
-        "parentSelectors": ["print-product-list"],
-        "selector": "a#hypUngrouped2",
-
-    },
-    {
-        "id": "table-row",
-        "type": "SelectorElement",
-        "parentSelectors": ["ungrouped-proceed"],
-        "selector": "tbody tr",
-
-    },
-    {
-        "id": "code",
-        "type": "SelectorText",
-        "parentSelectors": ["table-row"],
-        "selector": "td:nth-of-type(2)",
-        "multiple": false,
-
-    },
-    {
-        "id": "barcode",
-        "type": "SelectorElementAttribute",
-        "parentSelectors": ["table-row"],
-        "selector": "td img",
-        "multiple": false,
-
-    }
-]
+    def barcodes(self):
+        make query string using cat_codes (same as with products)
+        qs_start = 'https://www.booker.co.uk/catalog/printbyplof.aspx?printtype=searchcategory&categoryname='
+        barcode_urls =[(f"{qs_start}{i}") for i in cat_codes]
+        for page in barcode_urls:
+            request = Request(url=page,callback=self.barcode_parse)
+            yield requests
+            
+            
+   def barcode(self):
+        get each barcode from page and add with items
+        pass
+        
