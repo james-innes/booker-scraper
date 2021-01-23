3d1830f4902ea3129c19091f70ec42aa90168a99
James Innes <34094735+james-innes@users.noreply.github.com>
2021-01-23 20:51:06 +0000

    Fix cats.py

--- cats.py
+++ cats.py
@@ -7,16 +7,17 @@ new_cats = []
 
 replacements = [
 	[r'and', '&'],
-    [r'.?professional', ''],
     [r'.?retail', ''],
+    [r'.?professional', ''],
     [r'dog', 'Dog Food'],
     [r'cat', 'Cat Food'],
     [r'Dishwasher and Washing Up', 'Washing Up']
 ]
 
 for cat in cats:
+    new_cat = cat[0]
     for find, replace in replacements:
-        new_cat = re.sub(find, replace, cat[0], flags=re.IGNORECASE)
+        new_cat = re.sub(find, replace, new_cat, flags=re.IGNORECASE)
     new_cats.append(new_cat)
 
 pd.DataFrame(new_cats, columns=["cat"]).to_csv('cats.csv', index=False)
