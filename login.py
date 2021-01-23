954db80049758b65945512baf86711fad8522614
James Innes <34094735+james-innes@users.noreply.github.com>
2021-01-23 20:40:26 +0000

    Updated Login for NEW Booker website

--- login.py
+++ login.py
@@ -1,6 +1,5 @@
 
-import os
-import time
+import os, time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -12,11 +11,14 @@ options = Options()
 options.headless = True
 driver = webdriver.Firefox(options=options)
 driver.get('https://www.booker.co.uk/home')
+
 driver.find_element_by_id('CustomerNumber').send_keys(os.getenv('BOOKER_ACCOUNT'), Keys.RETURN)
-time.sleep(0.5)
+time.sleep(2)
 driver.find_element_by_id('Email').send_keys(os.getenv('BOOKER_EMAIL'))
 driver.find_element_by_id('Password').send_keys(os.getenv('BOOKER_PASSWORD'), Keys.RETURN)
-session = driver.get_cookie('ASP.NET_SessionId')['value']
-driver.quit()
 
-print(session)
+time.sleep(3)
+print("ASP_NET_SESSION: " + driver.get_cookie('ASP.NET_SessionId')['value'])
+print("ASPXAUTH: " + driver.get_cookie('.ASPXAUTH')['value'])
+
+driver.quit()
