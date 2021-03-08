34ec49906bfc5b54833c69926fcdec754eca5adf
James Innes <34094735+james-innes@users.noreply.github.com>
2021-03-08 19:48:11 +0000

    Remove barcdoe from login and rename driver to "d"

--- login.py
+++ login.py
@@ -9,22 +9,15 @@ load_dotenv()
 print("Be patient :)")
 options = Options()
 options.headless = True
-driver = webdriver.Firefox(options=options)
-driver.get('https://www.booker.co.uk/home')
+d = webdriver.Firefox(options=options)
+d.get('https://www.booker.co.uk/home')
 
-driver.find_element_by_id('CustomerNumber').send_keys(os.getenv('BOOKER_ACCOUNT'), Keys.RETURN)
+d.find_element_by_id('CustomerNumber').send_keys(os.getenv('BOOKER_ACCOUNT'), Keys.RETURN)
 time.sleep(2)
-driver.find_element_by_id('Email').send_keys(os.getenv('BOOKER_EMAIL'))
-driver.find_element_by_id('Password').send_keys(os.getenv('BOOKER_PASSWORD'), Keys.RETURN)
+d.find_element_by_id('Email').send_keys(os.getenv('BOOKER_EMAIL'))
+d.find_element_by_id('Password').send_keys(os.getenv('BOOKER_PASSWORD'), Keys.RETURN)
 time.sleep(3)
+print("ASP_NET_SESSION: " + d.get_cookie('ASP.NET_SessionId')['value'])
+print("ASPXAUTH: " + d.get_cookie('.ASPXAUTH')['value'])
 
-# Elevate permissions for Barcode page
-driver.get('https://www.booker.co.uk/products/product-list?categoryName=CS13_100001')
-driver.find_element_by_css_selector('a.print-btn').click()
-driver.find_element_by_css_selector('a[href*="print-product-list-ungroup"]').click()
-
-time.sleep(1)
-print("ASP_NET_SESSION: " + driver.get_cookie('ASP.NET_SessionId')['value'])
-print("ASPXAUTH: " + driver.get_cookie('.ASPXAUTH')['value'])
-
-driver.quit()
+d.quit()
