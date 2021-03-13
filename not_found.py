b6d885f7aec0788de09ad498b5710c34ba0ca8a7
James Innes <34094735+james-innes@users.noreply.github.com>
2021-03-13 16:14:36 +0000

    Revert not_found  to using csv instead of DB

--- not_found.py
+++ not_found.py
@@ -1,11 +1,8 @@
 import requests
 import sqlite3
+import pandas as pd
 
-con = sqlite3.connect('stores.db')
-cur = con.cursor()
-product_detail = cur.execute("SELECT * FROM product_detail").fetchall()
-con.close()
-
+product_detail = sqlite3.connect('stores.db').execute("SELECT * FROM product_detail").fetchall()
 not_found = []
 
 for product in product_detail:
@@ -15,13 +12,7 @@ for product in product_detail:
     r = requests.get(f"https://www.booker.co.uk/bbimages{img}")
 
     if r.status_code == 404:
-        print(f"Not Found for code: {code}")
-        not_found.append([code])
+      print(f"Not Found for code: {code}")
+      not_found.append(code)
 
-con = sqlite3.connect('stores.db')
-cur = con.cursor()
-cur.execute("DROP TABLE IF EXISTS not_found")
-cur.execute("CREATE TABLE not_found(code INTEGER)")
-cur.executemany("INSERT INTO not_found(code) VALUES(?)", not_found)
-con.commit()
-con.close()
+pd.DataFrame(not_found, columns=["code"]).to_csv('notfound.csv', index=False)
