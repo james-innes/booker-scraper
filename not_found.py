edcb9643261e1893e9f792f5c1f0ee4fe6fa8f30
James Innes <34094735+james-innes@users.noreply.github.com>
2021-03-13 15:25:47 +0000

    Inital not_found DB intermediary view won't show missing images

---
+++ not_found.py
@@ -0,0 +1,26 @@
+import requests
+import sqlite3
+
+con = sqlite3.connect('stores.db')
+cur = con.cursor()
+product_detail = cur.execute("SELECT * FROM product_detail").fetchall()
+con.close()
+
+not_found = []
+
+for product in product_detail:
+    code = product[0]
+    img = product[1]
+
+    r = requests.get(f"https://www.booker.co.uk/bbimages{img}")
+
+    if r.status_code == 404:
+        print(f"Not Found for code: {code}")
+        not_found.append([code])
+
+cur = con.cursor()
+cur.execute("DROP TABLE IF EXISTS not_found")
+cur.execute("CREATE TABLE not_found(code INTEGER)")
+cur.executemany("INSERT INTO not_found(code) VALUES(?)", not_found)
+con.commit()
+con.close()
