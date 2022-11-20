f8fe197e868c5044046451d6d20b1f6cb21bdd47
James Innes <34094735+james-innes@users.noreply.github.com>
2022-11-20 19:10:35 +0000

    update login screen

--- login.py
+++ login.py
@@ -1,26 +1,28 @@
-
 import os
 import time
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 
 load_dotenv()
 print("Be patient :)")
 options = Options()
-options.headless = True
-d = webdriver.Firefox(options=options)
-d.get('https://www.booker.co.uk/home')
+# options.headless = True
+d = webdriver.Chrome(options=options)
+d.get("https://www.booker.co.uk/home")
 
-d.find_element_by_id('CustomerNumber').send_keys(
-    os.getenv('BOOKER_ACCOUNT'), Keys.RETURN)
+d.find_element(by=By.ID, value="CustomerNumber").send_keys(
+    os.getenv("BOOKER_ACCOUNT"), Keys.RETURN
+)
 time.sleep(2)
-d.find_element_by_id('Email').send_keys(os.getenv('BOOKER_EMAIL'))
-d.find_element_by_id('Password').send_keys(
-    os.getenv('BOOKER_PASSWORD'), Keys.RETURN)
+d.find_element(by=By.ID, value="Email").send_keys(os.getenv("BOOKER_EMAIL"))
+d.find_element(by=By.ID, value="Password").send_keys(
+    os.getenv("BOOKER_PASSWORD"), Keys.RETURN
+)
 time.sleep(3)
-print("ASP_NET_SESSION: " + d.get_cookie('ASP.NET_SessionId')['value'])
-print("ASPXAUTH: " + d.get_cookie('.ASPXAUTH')['value'])
+print("ASP_NET_SESSION: " + d.get_cookie("ASP.NET_SessionId")["value"])
+print("ASPXAUTH: " + d.get_cookie(".ASPXAUTH")["value"])
 
 d.quit()
